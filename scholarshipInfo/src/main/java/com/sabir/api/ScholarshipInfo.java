c181afec1cf74724b00d257bf66cfd8215f6e2fe
mkhan <mkhan@zoomcare.com>
2017-06-14 09:07:31 +0530

    Starting Project
    Starting Project

---
+++ scholarshipInfo/src/main/java/com/sabir/api/ScholarshipInfo.java
@@ -0,0 +1,15 @@
+package com.sabir.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ScholarshipInfo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarshipInfo.class, args).close();
+
+	}
+
+}
